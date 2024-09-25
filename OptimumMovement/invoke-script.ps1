ae16774bf10bfa9400958d35cfed14097c102845
itsolver <angus@itsolver.net>
2024-09-25 14:37:40 +1000

    Rename Remove-OutlookNew to OM-LogonScript and update script URLs

--- OptimumMovement/invoke-script.ps1
+++ OptimumMovement/invoke-script.ps1
@@ -1,4 +1,4 @@
-$githubScriptUrl = 'https://raw.githubusercontent.com/itsolver/LogonScripts/refs/heads/main/Remove-OutlookNew.ps1'
+$githubScriptUrl = 'https://raw.githubusercontent.com/itsolver/LogonScripts/refs/heads/main/OptimumMovement/OM-LogonScript.ps1'
 $regKeyLocation = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
 $psCommand = "PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -Command `"& {Invoke-RestMethod '$githubScriptUrl' | Invoke-Expression}`""
 
@@ -6,7 +6,7 @@ if (-not(Test-Path -Path $regKeyLocation)) {
     New-Item -Path $regKeyLocation -Force | Out-Null
 }
 
-Set-ItemProperty -Path $regKeyLocation -Name 'Remove-OutlookNew' -Value $psCommand -Force
+Set-ItemProperty -Path $regKeyLocation -Name 'OM-LogonScript' -Value $psCommand -Force
 
 # Invoke the script immediately
 Invoke-Expression -Command "& {Invoke-RestMethod '$githubScriptUrl' | Invoke-Expression}"
